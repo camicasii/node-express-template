310cb7cd3989de0daa8c94ac3c8709b5246c3872
camicasii <camicasii@hotmail.com>
2019-11-10 20:35:28 -0400

    mongoose configurado

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -3,18 +3,27 @@ import {User_}  from "../database/mongodb/Schema";
 
 const route = Router();
 
-route.post('/',(req, res)=>{
-    console.log(req);
-    return res.send("ready")
+route.post('/',async(req, res)=>{
+    const data = req.body;
+    const username = data.username;
+    const password = data.password;
+    if(username===undefined ||password===undefined) return res.status(402).json("status:campo invalido");
+    const isUser = await User_.find({username:username})
+    if(isUser.length<1  ){        
+        const newUser = new User_(data);
+        await newUser.save();        
+        return res.status(200).json("status:Create User")
+    }
+    else{
+        return res.status(401).json("status:User Or password invalidate")
+    }
 })
 
 
 
 route.get('/',async (req, res)=>{    
-    const a = await User_.find()
-    console.log(a);
-    
-    return res.send("ready")
+    const data = await User_.find()
+    return res.json({data})
 })
 
 export default route;
